89c2aea3651b9279992047733760204c58072e1f
Celina <celinathestar2005@gmail.com>
2022-07-23 19:38:01 +0300

    added the files

---
+++ proj.js
@@ -0,0 +1,24 @@
+arr= new Array();
+arr[0]=prompt("first task here","no task -left empty-");
+
+arr[1]=prompt('second task here','no task -left empty-');
+
+arr[2]=prompt('third task here','no task -left empty-');
+
+arr[3]=prompt('fourth task here','no task -left empty-');
+
+arr[4]=prompt('fifth task here','no task -left empty-');
+
+alert("Don't overload your schedule! 5 tasks are enough.");
+
+var a=arr[0]
+var b=arr[1]
+var c=arr[2]
+var d=arr[3]
+var e=arr[4] 
+
+document.getElementById("11").innerHTML=a
+document.getElementById("22").innerHTML=b
+document.getElementById("33").innerHTML=c
+document.getElementById("44").innerHTML=d
+document.getElementById("55").innerHTML=e        
